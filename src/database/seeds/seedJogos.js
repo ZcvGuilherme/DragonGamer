adaa4cc276f8522092d9f28617af250f8172f536
IsaquePFerreira <pferreiraisaque@gmail.com>
2026-02-10 09:19:08 -0300

    Adiciona url das capas do jogos

--- src/database/seeds/seedJogos.js
+++ src/database/seeds/seedJogos.js
@@ -6,25 +6,25 @@ export async function seedJogos() {
       nome: 'The Legend of Zelda',
       ano: 1986,
       categoria: 'Aventura',
-      capaUrl: 'https://example.com/zelda.jpg'
+      capaUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Zelda_Logo.svg/1920px-Zelda_Logo.svg.png'
     },
     {
       nome: 'Hollow Knight',
       ano: 2017,
       categoria: 'Plataforma',
-      capaUrl: 'https://example.com/hollow-knight.jpg'
+      capaUrl: 'https://upload.wikimedia.org/wikipedia/pt/a/a1/Hollow_Knight_Cover.jpg'
     },
     {
       nome: 'Celeste',
       ano: 2018,
       categoria: 'Plataforma',
-      capaUrl: 'https://example.com/celeste.jpg'
+      capaUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Celeste_box_art_cropped.png'
     },
     {
       nome: 'God of War',
       ano: 2018,
       categoria: 'Ação',
-      capaUrl: 'https://example.com/god-of-war.jpg'
+      capaUrl: 'https://upload.wikimedia.org/wikipedia/pt/4/4b/God_of_War_2018_logo.png'
     }
   ];
 
